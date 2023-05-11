6828bc9ea5fd16cadad0c94280bd991565f41801
Pham Duc Binh <binh1298@gmail.com>
2023-05-11 13:42:56 +0700

    fix: minor rearrangement

--- simple/forest/scripts/powermenu.sh
+++ simple/forest/scripts/powermenu.sh
@@ -59,7 +59,7 @@ case $chosen in
         ;;
     $lock)
 		if [[ -f /usr/bin/i3lock ]]; then
-			i3lock
+			i3lock -c 031F26
 		elif [[ -f /usr/bin/betterlockscreen ]]; then
 			betterlockscreen -l
 		fi
